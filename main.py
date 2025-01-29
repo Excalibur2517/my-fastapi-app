fe96fe0b53932d773ee5c2c698dbe2ec4e46b02d
Excalibur2517 <artur@kashirokov.ru>
2025-01-29 22:21:31 +0300

    ver70.0

--- main.py
+++ main.py
@@ -840,6 +840,36 @@ def get_random_top_200_cartoon():
         cursor.close()
         conn.close()
 
+
+# Новый эндпоинт для получения 10 фильмов с наименьшим количеством символов в name
+@app.get("/cartoon/10_shortest_collections_list/", response_model=List[dict])
+def get_shortest_names():
+    """
+    Возвращает 10 фильмов с наименьшим количеством символов в поле name.
+    """
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    try:
+        # SQL-запрос для получения 10 строк с наименьшим количеством символов в поле name
+        cursor.execute("""
+            SELECT id, name, poster 
+            FROM cartoons_collections 
+            ORDER BY LENGTH(name) ASC
+            LIMIT 10
+        """)
+        
+        rows = cursor.fetchall()
+        
+        # Проверка на пустой результат
+        if not rows:
+            raise HTTPException(status_code=404, detail="No films found")
+        
+        return rows
+    except Error as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        cursor.close()
+        conn.close()
 #----------------------------------------Анимационные Сериалы---------------------------------
 # Эндпоинт для получения 20 случайных фильмов из топ-200 по популярности
 @app.get("/serials_animated/random_top200/")
