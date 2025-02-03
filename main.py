0fdcf17bf7ab4b13900a1252bf845282cdb13074
Excalibur2517 <artur@kashirokov.ru>
2025-02-03 17:26:52 +0300

    ver112.0

--- main.py
+++ main.py
@@ -1234,44 +1234,33 @@ def get_random_top_200_anime():
         cursor.close()
         conn.close()
 
-
-
-
-#------------------------------------КНИГИ----------------------------------------------
-# Новый эндпоинт для получения КАТАЛОГ 1
-@app.get("/books/classes")
-def get_distinct_classes():
+# Новый эндпоинт для получения 10 фильмов с наименьшим количеством символов в name
+@app.get("/anime/10_shortest_collections_list/", response_model=List[dict])
+def get_shortest_names():
+    """
+    Возвращает 10 фильмов с наименьшим количеством символов в поле name.
+    """
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
     try:
-        # Устанавливаем соединение с базой данных
-        conn = get_db_connection()
-        cursor = conn.cursor()
-
-        # Выполняем запрос
-        query = """
-        SELECT DISTINCT class_basic, class_level_2, class_level_3
-        FROM books_catalog bc
-        """
-        cursor.execute(query)
+        # SQL-запрос для получения 10 строк с наименьшим количеством символов в поле name
+        cursor.execute("""
+            SELECT id, name, poster 
+            FROM anime_collections 
+            ORDER BY LENGTH(name) ASC
+            LIMIT 10
+        """)
         
-        # Получаем результаты
-        results = cursor.fetchall()
-        classes = [
-            {
-                "class_basic": row[0],
-                "class_level_2": row[1],
-                "class_level_3": row[2]
-            }
-            for row in results
-        ]
-
-        # Закрываем соединение
+        rows = cursor.fetchall()
+        
+        # Проверка на пустой результат
+        if not rows:
+            raise HTTPException(status_code=404, detail="No films found")
+        
+        return rows
+    except Error as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
         cursor.close()
         conn.close()
 
-        # Возвращаем результаты
-        return {"classes": classes}
-
-    except Error as e:
-        raise HTTPException(status_code=500, detail=f"Ошибка при работе с базой данных: {str(e)}")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Неожиданная ошибка: {str(e)}")
